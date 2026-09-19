c06f3b40bd430e55d98ea90fb1f122f23207be35
Everflow <54564601+EverflowCN@users.noreply.github.com>
2026-09-20 03:08:01 +0800

    Switch public site to custom API domain

--- app.js
+++ app.js
@@ -246,7 +246,7 @@ const resources = [
   }
 ];
 
-const API_BASE = "https://zuotiben-api.bm9h54b4t9.workers.dev";
+const API_BASE = "https://api.zuotiben.top";
 
 function mapApiResource(item) {
   return {
